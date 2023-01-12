172d718c075ff1464cc3f849dfb085cffd809e9a
= <vnutalap@umd.edu>
2023-01-11 21:40:01 -0500

    changed

--- MovieTicketSales/salesapp/constants.py
+++
@@ -1,8 +0,0 @@
-customer_perm = ["customerHome", "getTheatres", "getAvailableSeats", "initiateBooking", "bookingSummary",
-                 "confirmBooking", "purchasehistory", "userPaymentInfo", "changeUserProfile"]
-
-theatre_perm = ["theatreHome", "addTheatre", "addMovieShow", "changeUserProfile", "viewticketsales", 
-                "viewticketsalesondate", "viewshows", "cancelshow"]
-
-admin_perm = ["viewtheatres", "viewticketsalesadmin", "viewcustomers", "changeUserProfile", 
-            "appsettings", "addmovie", "purchasehistory", "archiveuser"]
